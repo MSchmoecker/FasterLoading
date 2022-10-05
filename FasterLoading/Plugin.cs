ea95e22260cf143b78d6ef0ba74e28acfc11964b
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-10-05 22:44:38 +0200

    feat: added config option for patches

--- FasterLoading/Plugin.cs
+++ FasterLoading/Plugin.cs
@@ -1,10 +1,12 @@
 ﻿using BepInEx;
 using BepInEx.Bootstrap;
+using BepInEx.Configuration;
 using FasterLoading.Patches;
 using HarmonyLib;
 
 namespace FasterLoading {
     [BepInPlugin(ModGuid, ModName, ModVersion)]
+    [BepInIncompatibility("dev.ontrigger.vpo")]
     public class Plugin : BaseUnityPlugin {
         public const string ModName = "FasterLoading";
         public const string ModGuid = "com.maxsch.valheim.FasterLoading";
@@ -13,20 +15,23 @@ namespace FasterLoading {
         public static Plugin Instance { get; private set; }
         private Harmony harmony;
 
+        private ConfigEntry<bool> PatchMinimap { get; set; }
+        private ConfigEntry<bool> PatchWorldGenThreading { get; set; }
+
         private void Awake() {
             Instance = this;
 
             Log.Init(Logger);
             harmony = new Harmony(ModGuid);
-        }
 
-        private void Start() {
-            bool vpo = Chainloader.PluginInfos.ContainsKey("dev.ontrigger.vpo");
+            PatchMinimap = Config.Bind("Patches", "Minimap Caching", true, "Writes the minimap texture to disk and reuse it on subsequent world loads. This significantly speeds up loading times. Requires a restart to take effect.");
+            PatchWorldGenThreading = Config.Bind("Patches", "Load World Threaded", true, "Patches the world generation to load partially threaded. This speeds up loading times a bit on small bases and more on large bases. Requires a restart to take effect.");
 
-            Log.LogDebug($"VPO active: {vpo}");
-
-            if (!vpo) {
+            if (PatchMinimap.Value) {
                 harmony.PatchAll(typeof(MinimapGenerationPatch));
+            }
+
+            if (PatchWorldGenThreading.Value) {
                 harmony.PatchAll(typeof(ThreadedWorldLoadingPatch));
             }
         }
